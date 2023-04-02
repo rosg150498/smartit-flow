abb0a1ae6ed29a8e193f0b8e8c3e91c27beb5dd6
rosg150498 <rosg150498@gmail.com>
2023-04-02 19:20:18 +0000

    Add docstring to run.py

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
+"""
+IT Issue Tracking Management System
+
+The script allows users to manage IT Issues
+"""
+
 issue_description = input("Enter issue description: ")
 
 contact_number = input("Enter contact number: ")
