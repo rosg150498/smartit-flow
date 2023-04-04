39a28a4f2380eed82f2c3eae4dc7cde84b0cb522
rosg150498 <rosg150498@gmail.com>
2023-04-04 17:41:00 +0000

    Add leading zeros if id is less than 100 or 10, consistent format to run.py

--- run.py
+++ run.py
@@ -12,6 +12,13 @@ contact_number = input("Enter contact number: ")
 
 issue_id_formatted = str(issue_id_counter).zfill(2)
 
+if len(issue_id_formatted) == 3:
+    issue_id_prefixed = '00' + issue_id_formatted
+elif len(issue_id_formatted) == 2:
+    issue_id_prefixed = '0' + issue_id_formatted
+else:
+    issue_id_prefixed = issue_id_formatted
+
 print("Issue ID:", issue_id_counter)
 
 print("User Issue:", issue_description)
