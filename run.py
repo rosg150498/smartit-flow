3778ee491d5c9f3f04aa57374ed7638459f2e2e3
rosg150498 <rosg150498@gmail.com>
2023-04-04 17:16:42 +0000

    Initalize counter for issue adding user issue id to run.py

--- run.py
+++ run.py
@@ -4,9 +4,13 @@ IT Issue Tracking Management System
 The script allows users to manage IT Issues
 """
 
+issue_id_counter = 1
+
 issue_description = input("Enter issue description: ")
 
 contact_number = input("Enter contact number: ")
+
+print("Issue ID:", issue_id_counter)
 
 print("User Issue:", issue_description)
 
