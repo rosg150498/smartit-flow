e1f77a614888c966a6541372e6570a76eacd1a18
rosg150498 <rosg150498@gmail.com>
2023-04-06 20:48:50 +0000

    Add google drive link to scope to run.py

--- run.py
+++ run.py
@@ -10,7 +10,8 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
-   "https://www.googleapis.com/auth/spreadsheets"
+   "https://www.googleapis.com/auth/spreadsheets",
+   "https://www.googleapis.com/auth/drive"
 ]
 
 CREDS = Credentials.from_service_account_file('smartitflow-credentials.json')
