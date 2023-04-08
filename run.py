9ce78d62080c4b40f2888403e7d98c1f76deabb6
rosg150498 <rosg150498@gmail.com>
2023-04-08 19:21:04 +0000

    Define search issues and add issues function for user requests to run.py

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 """
 IT Issue Tracking Management System
-
 The script allows users to manage IT Issues
 """
 
@@ -33,40 +32,73 @@ def get_max_id():
     return int(max_id)
 
 
-worksheet = SHEET.worksheet('Issue')
-issue_id_counter = get_max_id() + 1
+def search_issues(search_term):
+    values = SHEET.worksheet('Issue').get_all_values()
 
-issue_description = input("Enter issue description: ")
+    matching_rows = [
+        row for row in values[1:] if
+        search_term.lower() in (row[0].lower(),
+                                row[3].lower())]
+
+    for row in matching_rows:
+        print(row)
+
+
+def add_issue():
+    global issue_id_counter
+    issue_id_counter = get_max_id() + 1
+
+    issue_description = input("Enter issue description: ")
+
+    while True:
+        contact_number = input("Enter contact number: ")
+        if contact_number.isdigit():
+            break
+        print("Invalid input. Please enter a number.")
+
+    categories = ["Hardware", "Software", "Network", "Other"]
+    print("IT Issue Category: ")
+    for i, category in enumerate(categories):
+        print(f"{i+1}. {category}")
+
+    category_choice = input("Enter the number for the category for the issue:")
+    while not category_choice.isdigit() or int(category_choice) < 1 or int(
+         category_choice) > len(categories):
+        category_choice = input("Invalid input. Enter the category number: ")
+
+    chosen_category = categories[int(category_choice)-1]
+
+    issue_date_created = datetime.now()
+
+    issue_id_formatted = str(issue_id_counter).zfill(3)
+
+    worksheet = SHEET.worksheet('Issue')
+    worksheet.append_row([
+        issue_id_formatted, 
+        chosen_category, 
+        issue_description, 
+        contact_number, 
+        issue_date_created.strftime("%d/%m/%Y %H:%M:%S")
+    ])
+
+    print("Issue has been added successfully!")
+
 
 while True:
-    contact_number = input("Enter contact number: ")
-    if contact_number.isdigit():
+    action = input('Enter action (search / add / quit): ')
+
+    if action == 'search':
+        search_term = input('Enter search term: ')
+        search_issues(search_term)
+
+    elif action == 'add':
+        add_issue()
+
+    elif action == 'quit':
         break
-    print("Invalid input. Please enter a number.")
 
-categories = ["Hardware", "Software", "Network", "Other"]
-print("IT Issue Category: ")
-for i, category in enumerate(categories):
-    print(f"{i+1}. {category}")
-
-category_choice = input("Enter the number for the category for the issue: ")
-while not category_choice.isdigit() or int(category_choice) < 1 or int(
-     category_choice) > len(categories):
-    category_choice = input("Invalid input. Enter the category number: ")
-
-chosen_category = categories[int(category_choice)-1]
-
-issue_date_created = datetime.now()
-
-issue_id_formatted = str(issue_id_counter).zfill(3)
-
-worksheet.append_row([
-    issue_id_formatted, 
-    chosen_category, 
-    issue_description, 
-    contact_number, 
-    issue_date_created.strftime("%d/%m/%Y %H:%M:%S")
-])
+    else: 
+        print("Invalid input. Please enter 'search', 'add', or 'quit'.")
 
 user_issues = SHEET.worksheet('Issue')
 
@@ -81,5 +113,3 @@ print("User Issue:", issue_description)
 print("User Contact Number:", contact_number)
 
 print("IT Issue Date Created:", issue_date_created)
-
-print(issues)
